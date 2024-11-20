ae8733e7b6d9022f8d2fe9d213933a9b14e69663
oscar calle <ocalle374@gmail.com>
2024-11-20 07:16:10 -0600

    cloning to laptop worked

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import Navbar from './components/header/Navbar';
 
 function App() {
   return (
-  <>
-  <Navbar/>
-  <Routes>
-            <Route path="/" element={<Home />} />
-        </Routes>
-  <h1>Hello</h1>
-  </>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <h1>Hello</h1>
+      <h2>Cloning to laptop worked</h2>
+    </>
   );
 }
 
